91ac860c6b69796e94b4869cefc8c40d3f1b0831
Ohsolution <deuslovelt3@gmail.com>
2020-10-01 15:39:12 +0900

    초안 완성

---
+++ hw1/work/ExprMain.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <cstdio>
+
+#include "antlr4-cpp/ExprBaseListener.h"
+#include "antlr4-cpp/ExprLexer.h"
+#include "antlr4-cpp/ExprParser.h"
+
+using namespace std;
+using namespace antlr4;
+using namespace antlr4::tree;
+
+#define DEFAULT 0
+#define ASSNPORT 1
+#define EXPRPORT 2
+
+#define P_FI 2
+#define P_SE 3
+
+struct term
+{
+    double val;
+    char op;
+    bool isop;
+};
+
+struct opr
+{
+    char op;
+    int pri;
+    bool lp,rp;
+
+    /*
+    void print()
+    {
+        cout << op << endl;
+        cout << pri << " " << lp <<" " << rp<< endl; 
+    }
+    */
+};
+
+class EvalListener : public ExprBaseListener {
+
+private:
+    int port= DEFAULT;
+    
+    /* ASSN SECTION */
+    string lid;
+    double rval;
+    unordered_map <string,double> xhash;
+
+    /*EXPR SECTION */
+
+    queue <term> bq;
+    stack <opr> opst;
+
+
+public:
+    
+
+	virtual void enterProg(ExprParser::ProgContext *ctx) {
+		cout << "enterProg: \n";
+	}
+	virtual void exitProg(ExprParser::ProgContext *ctx) {
+		cout << "exitProg: \n";
+	}
+	virtual void enterExpr(ExprParser::ExprContext *ctx) {
+        openport(EXPRPORT);
+		cout << "\tenterExpr: \n";
+	}
+	virtual void exitExpr(ExprParser::ExprContext *ctx) {
+		cout << "\texitExpr: \n";
+	}
+
+    virtual void enterAssn(ExprParser::AssnContext *ctx)
+    {
+        cout << "\tenterAssn: \n";
+        openport(ASSNPORT);
+    }
+
+    virtual void exitAssn(ExprParser::AssnContext *ctx)
+    {
+        xhash[lid] = rval;
+    }
+
+	virtual void visitTerminal(tree::TerminalNode *node) {
+		cout << "\t\tTerminal: " << node->getText() << "\n";
+
+        int st = node->getSymbol()->getType();
+        
+        if(st == ExprLexer::SEMI) closeport();
+
+        if(port == ASSNPORT)    
+        {
+            if(st == ExprLexer::ID) lid = node->getText();
+            if(st == ExprLexer::INT || st == ExprLexer::REAL) rval = stod(node->getText());            
+        }
+        else if(port == EXPRPORT)
+        {
+            if(st == ExprLexer::INT || st == ExprLexer::REAL) bq.push({stod(node->getText()),' ',0});
+            else if(st == ExprLexer::ID) bq.push({xhash[node->getText()],' ',0});
+            else
+            {
+                opr tmp;
+                tmp.lp = (st == ExprLexer::LP);
+                tmp.rp = (st == ExprLexer::RP);
+                tmp.op = (node->getText())[0];
+                tmp.pri =  (st==ExprLexer::PLUSM) * P_FI + (st==ExprLexer::MULD) * P_SE;
+
+                push_op(tmp);                
+            }
+        }            
+	}
+
+    void openport(int ty){ port = ty;}
+    void closeport(void)
+    {
+        if(port == EXPRPORT) calc();
+        port = DEFAULT;
+    }
+
+    void push_op(opr elem)
+    {
+        if(opst.empty() || elem.lp || elem.pri > opst.top().pri) 
+        {
+            opst.push(elem);
+            return;
+        }
+
+        if(elem.rp)
+        {
+            while(!opst.top().lp)
+            {
+                bq.push({0,opst.top().op,1});
+                opst.pop();    
+            }
+
+            opst.pop();
+
+            return;
+        }
+
+        while(opst.size() && elem.pri <= opst.top().pri)
+        {        
+            bq.push({0,opst.top().op,1});
+            opst.pop();
+        }
+
+        opst.push(elem);
+        return;
+    }
+
+    void calc()
+    {
+        while(!opst.empty())
+        {
+            bq.push({0,opst.top().op,1});
+            opst.pop();
+        }
+
+        vector <double> vd;
+
+        while(!bq.empty())
+        {
+            term cur = bq.front(); bq.pop();
+
+            if(!cur.isop)
+            {
+                cout << "el : " << cur.val << endl;
+                vd.push_back(cur.val);
+                continue;
+            }            
+
+            double s_val = vd.back(); vd.pop_back();
+            double f_val = vd.back(); vd.pop_back();
+            double ret;
+
+            switch (cur.op)
+            {
+                case '+':
+                    ret = f_val + s_val;
+                    break;
+                case '-':
+                    ret = f_val - s_val;
+                    break;
+                case '*':
+                    ret = f_val * s_val;
+                    break;
+                case '/':
+                    ret = f_val / s_val;
+                    break;
+                default:
+                    break;                
+            }
+
+            vd.push_back(ret);
+        }
+        
+        
+        printf("%.1lf",vd.back());
+    }
+};
+
+int main(int argc, const char* argv[]) 
+{
+	if (argc < 2) {
+		cerr << "[Usage] " << argv[0] << "  <input-file>\n";
+		exit(0);
+	}
+
+	std::ifstream stream;
+	stream.open(argv[1]);
+	if (stream.fail()) {
+		cerr << argv[1] << " : file open fail\n";
+		exit(0);
+	}
+
+	cout << "---Expression Evaluation with ANTLR listener---\n";
+	ANTLRInputStream input(stream);
+	ExprLexer lexer(&input);
+	CommonTokenStream tokens(&lexer);
+	ExprParser parser(&tokens);	
+    //ParseTree *tree = parser.prog();
+
+    //cout << tree->toStringTree(&parser) << endl;
+
+    ParseTreeWalker walker;
+	EvalListener listener;	
+	walker.walk(&listener, parser.prog());
+    return 0;
+}
---
+++ hw1/work/antlr4-cpp/ExprParser.h
@@ -0,0 +1,128 @@
+
+// Generated from Expr.g4 by ANTLR 4.8
+
+#pragma once
+
+
+#include "antlr4-runtime.h"
+
+
+
+
+class  ExprParser : public antlr4::Parser {
+public:
+  enum {
+    T__0 = 1, NEWLINE = 2, INT = 3, REAL = 4, MULD = 5, PLUSM = 6, ID = 7, 
+    WS = 8, LP = 9, RP = 10, SEMI = 11
+  };
+
+  enum {
+    RuleProg = 0, RuleExpr = 1, RuleAssn = 2, RuleNum = 3
+  };
+
+  ExprParser(antlr4::TokenStream *input);
+  ~ExprParser();
+
+  virtual std::string getGrammarFileName() const override;
+  virtual const antlr4::atn::ATN& getATN() const override { return _atn; };
+  virtual const std::vector<std::string>& getTokenNames() const override { return _tokenNames; }; // deprecated: use vocabulary instead.
+  virtual const std::vector<std::string>& getRuleNames() const override;
+  virtual antlr4::dfa::Vocabulary& getVocabulary() const override;
+
+
+  class ProgContext;
+  class ExprContext;
+  class AssnContext;
+  class NumContext; 
+
+  class  ProgContext : public antlr4::ParserRuleContext {
+  public:
+    ProgContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    std::vector<AssnContext *> assn();
+    AssnContext* assn(size_t i);
+    std::vector<antlr4::tree::TerminalNode *> SEMI();
+    antlr4::tree::TerminalNode* SEMI(size_t i);
+    std::vector<ExprContext *> expr();
+    ExprContext* expr(size_t i);
+    std::vector<antlr4::tree::TerminalNode *> NEWLINE();
+    antlr4::tree::TerminalNode* NEWLINE(size_t i);
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  ProgContext* prog();
+
+  class  ExprContext : public antlr4::ParserRuleContext {
+  public:
+    ExprContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    NumContext *num();
+    antlr4::tree::TerminalNode *ID();
+    antlr4::tree::TerminalNode *LP();
+    std::vector<ExprContext *> expr();
+    ExprContext* expr(size_t i);
+    antlr4::tree::TerminalNode *RP();
+    antlr4::tree::TerminalNode *MULD();
+    antlr4::tree::TerminalNode *PLUSM();
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  ExprContext* expr();
+  ExprContext* expr(int precedence);
+  class  AssnContext : public antlr4::ParserRuleContext {
+  public:
+    AssnContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    antlr4::tree::TerminalNode *ID();
+    NumContext *num();
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  AssnContext* assn();
+
+  class  NumContext : public antlr4::ParserRuleContext {
+  public:
+    NumContext(antlr4::ParserRuleContext *parent, size_t invokingState);
+    virtual size_t getRuleIndex() const override;
+    antlr4::tree::TerminalNode *INT();
+    antlr4::tree::TerminalNode *REAL();
+
+    virtual void enterRule(antlr4::tree::ParseTreeListener *listener) override;
+    virtual void exitRule(antlr4::tree::ParseTreeListener *listener) override;
+   
+  };
+
+  NumContext* num();
+
+
+  virtual bool sempred(antlr4::RuleContext *_localctx, size_t ruleIndex, size_t predicateIndex) override;
+  bool exprSempred(ExprContext *_localctx, size_t predicateIndex);
+
+private:
+  static std::vector<antlr4::dfa::DFA> _decisionToDFA;
+  static antlr4::atn::PredictionContextCache _sharedContextCache;
+  static std::vector<std::string> _ruleNames;
+  static std::vector<std::string> _tokenNames;
+
+  static std::vector<std::string> _literalNames;
+  static std::vector<std::string> _symbolicNames;
+  static antlr4::dfa::Vocabulary _vocabulary;
+  static antlr4::atn::ATN _atn;
+  static std::vector<uint16_t> _serializedATN;
+
+
+  struct Initializer {
+    Initializer();
+  };
+  static Initializer _init;
+};
+
